877f6da4d1ba6aaef54e6b4162d9b0edb33266ee
SO <omarbagaev@gmail.com>
2019-08-07 20:58:53 +1200

    - MongoDB connected;

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
+
+//Connect DB
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.json({
