c68af1938224aecf30957963e9218af3c219793a
SO <omarbagaev@gmail.com>
2019-08-14 07:09:44 +1200

    - added jwt(token);

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const {
     check,
     validationResult
-} = require('express-validator');
+} = require('express-validator/check');
 
 const User = require('../models/User');
 
@@ -66,6 +68,19 @@ router.post(
                     id: user.id
                 }
             };
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'), {
+                    expiresIn: 360000
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({
+                        token
+                    });
+                }
+            );
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server error');
