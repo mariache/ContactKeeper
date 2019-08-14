a5c33540d54b90fa3c17840954c3541af2de80af
SO <omarbagaev@gmail.com>
2019-08-14 19:17:37 +1200

    - get loged in user;

--- routes/auth.js
+++ routes/auth.js
@@ -2,20 +2,27 @@ const express = require('express')
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const auth = require('../middleware/auth');
 const config = require('config');
 const {
     check,
     validationResult
-} = require('express-validator/check');
+} = require('express-validator');
 
 const User = require('../models/User');
 
 //@route   GET api/auth
 //@desc    Get logged in user
 //@access  Private
-router.get('/', (req, res) => {
-    res.send('Get logged in user')
-})
+router.get('/', auth, async(req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 //@route   POST api/auth
 //@desc    Auth user & get token
--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const config = require('config');
 const {
     check,
     validationResult
-} = require('express-validator/check');
+} = require('express-validator');
 
 const User = require('../models/User');
 
