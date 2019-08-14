188c3a4d071b12f1b01ffe76af3fcfd0d4ae77f7
SO <omarbagaev@gmail.com>
2019-08-14 19:52:39 +1200

    - add contacts;

--- routes/contacts.js
+++ routes/contacts.js
@@ -28,9 +28,51 @@ router.get('/', auth, async(req, res) => {
 //@route   POST api/contacts
 //@desc    Get all users Contact
 //@access  Private
-router.post('/', (req, res) => {
-    res.send('Add contact')
-})
+router.post(
+    '/', [
+        auth, [
+            check('name', 'Name is required')
+            .not()
+            .isEmpty(),
+            check('type', 'Type must be personal or professional').isIn([
+                'personal',
+                'professional'
+            ])
+        ]
+    ],
+    async(req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        const {
+            name,
+            email,
+            phone,
+            type
+        } = req.body;
+
+        try {
+            const newContact = new Contact({
+                name,
+                email,
+                phone,
+                type,
+                user: req.user.id
+            });
+
+            const contact = await newContact.save();
+
+            res.json(contact);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
 
 
 //@route   PUT api/contacts.:id
