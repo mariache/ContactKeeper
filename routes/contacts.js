127715f6b10fda5a14409d862db4244ecc6c2948
SO <omarbagaev@gmail.com>
2019-08-14 20:10:44 +1200

    - remove contact;

--- routes/contacts.js
+++ routes/contacts.js
@@ -131,8 +131,30 @@ router.put('/:id', auth, async(req, res) => {
 //@route   DELETE api/contacts/:id
 //@desc    Delete Contact
 //@access  Private
-router.delete('/:id', (req, res) => {
-    res.send('Delete contact')
-})
+router.delete('/:id', auth, async(req, res) => {
+    try {
+        const contact = await Contact.findById(req.params.id);
+
+        if (!contact) return res.status(404).json({
+            msg: 'Contact not found'
+        });
+
+        // Make sure user owns contact
+        if (contact.user.toString() !== req.user.id)
+            return res.status(401).json({
+                msg: 'Not authorized'
+            });
+
+        await Contact.findByIdAndRemove(req.params.id);
+
+        res.json({
+            msg: 'Contact removed'
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
 
 module.exports = router;
