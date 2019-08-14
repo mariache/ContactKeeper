59a1822cfd2e86445a4d188a24d9aa9c5d60ec2b
SO <omarbagaev@gmail.com>
2019-08-14 20:08:12 +1200

    - update contact;

--- routes/contacts.js
+++ routes/contacts.js
@@ -78,9 +78,54 @@ router.post(
 //@route   PUT api/contacts.:id
 //@desc    Update Contact
 //@access  Private
-router.put('/:id', (req, res) => {
-    res.send('Update contact')
-})
+router.put('/:id', auth, async(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+        return res.status(400).json({
+            errors: errors.array()
+        });
+
+    const {
+        name,
+        email,
+        phone,
+        type
+    } = req.body;
+
+    // Build contact object
+    const contactFields = {};
+    if (name) contactFields.name = name;
+    if (email) contactFields.email = email;
+    if (phone) contactFields.phone = phone;
+    if (type) contactFields.type = type;
+
+    try {
+        let contact = await Contact.findById(req.params.id);
+
+        if (!contact) return res.status(404).json({
+            msg: 'Contact not found'
+        });
+
+        // Make sure user owns contact
+        if (contact.user.toString() !== req.user.id)
+            return res.status(401).json({
+                msg: 'Not authorized'
+            });
+
+        contact = await Contact.findByIdAndUpdate(
+            req.params.id, {
+                $set: contactFields
+            }, {
+                new: true
+            }
+        );
+
+        res.json(contact);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
 
 
 //@route   DELETE api/contacts/:id
