cfb732934084c1d676ba8519205040a0395036dd
SO <omarbagaev@gmail.com>
2019-08-20 00:38:54 +1200

    - finished contact's tile;

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useContext } from "react";
 import ContactContext from "../../components/context/contact/contactContext";
+import ContactItem from "./ContactItem";
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
@@ -7,7 +8,7 @@ const Contacts = () => {
   return (
     <Fragment>
       {contacts.map(contact => (
-        <h3>{contact.name}</h3>
+        <ContactItem key={contact.id} contact={contact} />
       ))}
     </Fragment>
   );
