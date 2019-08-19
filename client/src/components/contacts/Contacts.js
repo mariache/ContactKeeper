924162d6034ae1f3cfa3ba708eb316e93fc74249
SO <omarbagaev@gmail.com>
2019-08-20 00:10:06 +1200

    - display contacts on home page;

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { Fragment, useContext } from "react";
+import ContactContext from "../../components/context/contact/contactContext";
 
 const Contacts = () => {
-  return <div />;
+  const contactContext = useContext(ContactContext);
+  const { contacts } = contactContext;
+  return (
+    <Fragment>
+      {contacts.map(contact => (
+        <h3>{contact.name}</h3>
+      ))}
+    </Fragment>
+  );
 };
 
 export default Contacts;
