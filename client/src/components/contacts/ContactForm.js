be06f9bdb2b55be6ffa67951770c800639a0da64
SO <omarbagaev@gmail.com>
2019-08-21 07:26:45 +1200

    - add contact form;

---
+++ client/src/components/contacts/ContactForm.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+const ContactForm = () => {
+  const [contact, setContact] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    type: "personal"
+  });
+
+  const { name, email, phone, type } = contact;
+
+  return;
+  <form>
+    <h2 className="text-primary">Add Contact</h2>
+    <input
+      type="text"
+      placeholder="name"
+      name="name"
+      value={name}
+      onChange={onChange}
+    />
+    <input
+      type="email"
+      placeholder="Email"
+      name="email"
+      value={email}
+      onChange={onChange}
+    />
+    <input
+      type="phone"
+      placeholder="Phone"
+      name="phone"
+      value={phone}
+      onChange={onChange}
+    />
+    <h5>
+      <input
+        type="radio"
+        name="type"
+        value="personal"
+        checked={type === "personal"}
+      />
+      Personal{""}
+      <input
+        type="radio"
+        name="type"
+        value="professional"
+        checked={type === "professional"}
+      />
+      Professional{""}
+    </h5>
+    <div>
+      <input
+        type="submit"
+        value="Add Contact"
+        className="btn btn-primary btn-block"
+      />
+    </div>
+  </form>;
+};
+
+export default ContactForm;
