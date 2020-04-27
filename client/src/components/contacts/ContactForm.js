e40c44c198e82021bdf299b5c63da705fa2c1474
Salam Omarbagaev <omarbagaev@gmail.com>
2020-04-27 23:37:47 +1200

    - component form layout;

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -5,59 +5,60 @@ const ContactForm = () => {
     name: "",
     email: "",
     phone: "",
-    type: "personal"
+    type: "personal",
   });
 
   const { name, email, phone, type } = contact;
 
-  return;
-  <form>
-    <h2 className="text-primary">Add Contact</h2>
-    <input
-      type="text"
-      placeholder="name"
-      name="name"
-      value={name}
-      onChange={onChange}
-    />
-    <input
-      type="email"
-      placeholder="Email"
-      name="email"
-      value={email}
-      onChange={onChange}
-    />
-    <input
-      type="phone"
-      placeholder="Phone"
-      name="phone"
-      value={phone}
-      onChange={onChange}
-    />
-    <h5>
+  return (
+    <form>
+      <h2 className="text-primary">Add Contact</h2>
       <input
-        type="radio"
-        name="type"
-        value="personal"
-        checked={type === "personal"}
+        type="text"
+        placeholder="name"
+        name="name"
+        value={name}
+        onChange={onChange}
       />
-      Personal
       <input
-        type="radio"
-        name="type"
-        value="professional"
-        checked={type === "professional"}
+        type="email"
+        placeholder="Email"
+        name="email"
+        value={email}
+        onChange={onChange}
       />
-      Professional
-    </h5>
-    <div>
       <input
-        type="submit"
-        value="Add Contact"
-        className="btn btn-primary btn-block"
+        type="phone"
+        placeholder="Phone"
+        name="phone"
+        value={phone}
+        onChange={onChange}
       />
-    </div>
-  </form>;
+      <h5>
+        <input
+          type="radio"
+          name="type"
+          value="personal"
+          checked={type === "personal"}
+        />
+        Personal
+        <input
+          type="radio"
+          name="type"
+          value="professional"
+          checked={type === "professional"}
+        />
+        Professional
+      </h5>
+      <div>
+        <input
+          type="submit"
+          value="Add contact"
+          className="btn btn-primary btn-block"
+        />
+      </div>
+    </form>
+  );
 };
 
 export default ContactForm;
