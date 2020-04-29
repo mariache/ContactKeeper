e14a3987adf7dd0a2ae8a6560ce4d2b7cfa275e9
Salam Omarbagaev <omarbagaev@gmail.com>
2020-04-29 23:34:47 +1200

    - started authentiication;

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,25 @@ import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import ContactState from "./components/context/contact/ContactState";
+import AuthState from "./components/context/auth/AuthState";
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
